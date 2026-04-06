5e252d98dfdd0ccb11780d1dc39b9d8e3b74a84c
wendel.pereira <jwend3l@gmail.com>
2026-04-06 14:54:24 -0300

    feat: adicionado expressões regulares para validação de senha dos DTOs

--- src/main/java/com/controlefinaneiro/api/usuario/dto/LoginDTO.java
+++ src/main/java/com/controlefinaneiro/api/usuario/dto/LoginDTO.java
@@ -2,6 +2,7 @@ package com.controlefinaneiro.api.usuario.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record LoginDTO(
@@ -12,6 +13,8 @@ public record LoginDTO(
 
      @Size(min =8)
      @NotBlank(message = "Senha é obrigatória.")
+     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$",
+             message = "A senha deve conter ao menos uma letra maiúscula, um número e um caractere especial.")
      String senha
     
     ) {}
--- src/main/java/com/controlefinaneiro/api/usuario/dto/UsuarioDTO.java
+++ src/main/java/com/controlefinaneiro/api/usuario/dto/UsuarioDTO.java
@@ -4,6 +4,7 @@ package com.controlefinaneiro.api.usuario.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UsuarioDTO(
@@ -17,10 +18,14 @@ public record UsuarioDTO(
 
     @Size(min = 8)
     @NotBlank(message = "Senha é obrigatória.")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$",
+             message = "A senha deve conter ao menos uma letra maiúscula, um número e um caractere especial.")
     String senha,
 
     @Size(min = 8)
     @NotBlank(message = "Campo de confirmação obrigatório.")
+     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$",
+             message = "A senha deve conter ao menos uma letra maiúscula, um número e um caractere especial.")
     String confirmarSenha
 
 ) {
