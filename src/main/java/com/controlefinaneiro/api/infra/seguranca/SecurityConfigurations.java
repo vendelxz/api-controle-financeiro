6b5474234cc946dc8409330a3a0c4aa99d88c15f
vendelxz <jwend3l@gmail.com>
2026-04-10 18:33:08 -0300

    hotfitx: URL front corrigida para habilitar CORS

--- src/main/java/com/controlefinaneiro/api/infra/seguranca/SecurityConfigurations.java
+++ src/main/java/com/controlefinaneiro/api/infra/seguranca/SecurityConfigurations.java
@@ -65,7 +65,7 @@ public class SecurityConfigurations {
         CorsConfiguration configuration = new CorsConfiguration();
         
         //Porta que vai rodar o front da API
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://127.0.0.1:5500", "https://vendelxz.github.io/finance.control/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://127.0.0.1:5500", "https://vendelxz.github.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
